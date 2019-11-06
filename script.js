c199d4c3e375d5768a3f14ca762fa684438ded08
Christophe Asselin <christophe.asselin@hotmail.com>
2019-11-06 15:28:17 -0500

    added dank celebration and custom cursor

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 const THE_DAY = new Date(2019, 10, 28, 0, 0, 0, 0).getTime();
 
-const now = (new Date).getTime();
+const LEFT_CELEBRATION_GIF_NAMES = ['banana.gif', 'datboi.gif'];
+const RIGHT_CELEBRATION_GIF_NAMES = ['snoop.gif', 'wow.gif'];
+const EXTRA_CELEBRATION_GIF_NAME = 'fireworks.gif';
+
+let isTimerVisible = true;
+
+const updateTimer = (timeDifference) => {
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('timer').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+};
+
+const letsCelebrate = () => {
+    document.getElementById('timer').innerHTML = '0d 0h 0m 0s';
+    clearInterval(timer);
+
+    const divs = document.getElementsByClassName('fireworks');
+    for (let i = 0; i < divs.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            const gif = document.createElement('img');
+            gif.src = 'assets/' + EXTRA_CELEBRATION_GIF_NAME;
+            divs[i].appendChild(gif);
+        }
+    }
+
+    for (let gifName of LEFT_CELEBRATION_GIF_NAMES) {
+        const gif = document.createElement('img');
+        gif.className = 'image';
+        gif.src = 'assets/' + gifName;
+        document.getElementById('left-celebration').appendChild(gif);
+    }
+
+    for (let gifName of RIGHT_CELEBRATION_GIF_NAMES) {
+        const gif = document.createElement('img');
+        gif.className = 'image';
+        gif.src = 'assets/' + gifName;
+        document.getElementById('right-celebration').appendChild(gif);
+    }
+
+    setInterval(() => {
+        document.getElementById('timer').style.visibility = isTimerVisible ? 'hidden' : 'visible';
+        isTimerVisible = !isTimerVisible;
+    }, 400);
+};
 
 const timerCallback = () => {
     const now = (new Date).getTime();
@@ -8,20 +54,12 @@ const timerCallback = () => {
     const timeDifference = THE_DAY - now;
 
     if (timeDifference > 0) {
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        document.getElementById('timer').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+        updateTimer(timeDifference);
     } else {
-        document.getElementById('timer').innerHTML = 'allo';
-        // add stuff
+        letsCelebrate();
     }
 };
 
-timerCallback();
-
 const timer = setInterval(timerCallback, 1000);
 
-
+timerCallback();
